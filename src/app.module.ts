44222ff879d401259de3e14afcd949dbba0fbb1c
Clinton Brito <clintoncsb@gmail.com>
2024-07-20 17:04:16 -0300

    ✨ feat: add TypeORM, SQLite3, and initial `Project` entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './projects/entities/project.entity';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'root',
+      database: ':memory:',
+      entities: [Project],
+      synchronize: true,
+    }),
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
