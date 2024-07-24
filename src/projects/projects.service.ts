615d5ac0a046f6983e48e056a173b8b2f3c6f523
Clinton Brito <clintoncsb@gmail.com>
2024-07-24 13:55:17 -0300

    ✨ feat: create `Projects` CRUD other methods and update DTOs

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -39,20 +39,56 @@ export class ProjectsService {
 
     if (updateProjectDto.started_at) {
       if (project.status === ProjectStatus.Active) {
-        throw new Error('Project already started');
+        throw new Error('You cannot start an active project');
       }
 
       if (project.status === ProjectStatus.Completed) {
-        throw new Error('Project already completed');
+        throw new Error('You cannot start a completed project');
       }
 
       if (project.status === ProjectStatus.Cancelled) {
-        throw new Error('Project already cancelled');
+        throw new Error('You cannot start a cancelled project');
       }
 
       project.started_at = updateProjectDto.started_at;
       project.status = ProjectStatus.Active;
     }
+
+    if (updateProjectDto.cancelled_at) {
+      if (project.status === ProjectStatus.Completed) {
+        throw new Error('You cannot cancel a completed project');
+      }
+
+      if (project.status === ProjectStatus.Cancelled) {
+        throw new Error('You cannot cancel a cancelled project');
+      }
+
+      if (updateProjectDto.cancelled_at < project.started_at) {
+        throw new Error('You cannot cancel a project before it starts');
+      }
+
+      project.cancelled_at = updateProjectDto.cancelled_at;
+      project.status = ProjectStatus.Cancelled;
+    }
+
+    if (updateProjectDto.finished_at) {
+      if (project.status === ProjectStatus.Completed) {
+        throw new Error('You cannot finish a completed project');
+      }
+
+      if (project.status === ProjectStatus.Cancelled) {
+        throw new Error('You cannot finish a cancelled project');
+      }
+
+      if (updateProjectDto.finished_at < project.started_at) {
+        throw new Error('You cannot finish a project before it starts');
+      }
+
+      project.finished_at = updateProjectDto.finished_at;
+      project.status = ProjectStatus.Completed;
+    }
+
+    return this.projectRepo.save(project);
   }
 
   remove(id: string) {
